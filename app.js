86821366474ba0a60206c4a257fd8deecd0a3a78
Çağdaş Aldoğan <cagdas.aldogan00@gmail.com>
2023-04-09 21:06:14 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+function connectDb() {
+  const connection = mysql.createConnection({
+    host: 'company-entrance.cmx8hjl5dfbg.eu-north-1.rds.amazonaws.com',
+    user: 'cagdasadmin',
+    password: 'cagdas24ald',
+    database: 'entrance_project'
+  });
+
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log('MySQL veritabanına bağlandı.');
+  });
+
+  return connection;
+}
+
+app.get('/gecis', (req, res) => {
+  const connection = connectDb();
+
+  connection.query('SELECT * FROM gecis', (err, rows) => {
+    if (err) throw err;
+
+    console.log('Gecis tablosu kayitlari:');
+    rows.forEach((row) => {
+      console.log(row);
+    });
+
+    res.send(rows);
+
+    connection.end((err) => {
+      if (err) throw err;
+      console.log('MySQL veritabanı bağlantısı kapatıldı.');
+    });
+  });
+});
+
+// Sunucuyu başlat
+app.listen(PORT, () => {
+  console.log(`Sunucu başlatıldı: http://localhost:${PORT}`);
+});
